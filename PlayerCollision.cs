a106df5435072484eca55055cd6e39a890b47423
aliitayem1026 <aliitayem1026@gmail.com>
2022-08-15 05:37:50 -0400

    Find And Reference GameManager Script From Others

--- PlayerCollision.cs
+++ PlayerCollision.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
-    public PlayerMovement movement;
+	public PlayerMovement movement;     // A reference to our PlayerMovement script
 
-    void OnCollisionEnter (Collision collisionInfo)
-    {
-        if (collisionInfo.collider.tag == "Obstacle")
-        {
-            movement.enabled = false;
-        }
-    }
+	// This function runs when we hit another object.
+	// We get information about the collision and call it "collisionInfo".
+	void OnCollisionEnter (Collision collisionInfo)
+	{
+		// We check if the object we collided with has a tag called "Obstacle".
+		if (collisionInfo.collider.tag == "Obstacle")
+		{
+			movement.enabled = false;   // Disable the players movement.
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
 
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
 
-    public Rigidbody rb; 
+	// This is a reference to the Rigidbody component called "rb"
+	public Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.useGravity = false;
-    }
- 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float forwardForce = 2000f;	// Variable that determines the forward force
+	public float sidewaysForce = 500f;  // Variable that determines the sideways force
+
+	// We marked this as "Fixed"Update because we
+	// are using it to mess with physics.
+	void FixedUpdate ()
+	{
+		// Add a forward force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey("d"))	// If the player is pressing the "d" key
+		{
+			// Add a force to the right
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey("a"))  // If the player is pressing the "a" key
+		{
+			// Add a force to the left
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -1f)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
 }
